2e8c9a07e072bd599683582fb8dc440e311b9e3d
ayoub gharbi <ayoub97ma@gmail.com>
2019-08-05 21:01:39 +0100

    player click

--- app/src/main/java/com/ginggfreccs/startup/MainActivity.kt
+++ app/src/main/java/com/ginggfreccs/startup/MainActivity.kt
@@ -2,11 +2,33 @@ package com.ginggfreccs.startup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buSelect(view:View){
+        val buChoise= view as Button
+        var cellID=0
+        when(buChoise.id){
+            R.id.bu1-> cellID=1
+            R.id.bu2-> cellID=2
+            R.id.bu4-> cellID=3
+            R.id.bu4-> cellID=4
+            R.id.bu5-> cellID=5
+            R.id.bu6-> cellID=6
+            R.id.bu7-> cellID=7
+            R.id.bu8-> cellID=8
+            R.id.bu9-> cellID=9
+        }
+        Log.d("cellID:",cellID.toString())
+
+        buChoise.setBackgroundResource(R.color.blue)
     }
 }
